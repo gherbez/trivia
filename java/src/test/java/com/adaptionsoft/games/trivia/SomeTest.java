09944abb5c9f52cba9bfe576ed822151288af606
Gregory HERBEZ <gregory.herbez@accenture.com>
2020-04-17 15:45:25 +0200

    Add some test

--- java/src/test/java/com/adaptionsoft/games/trivia/SomeTest.java
+++ java/src/test/java/com/adaptionsoft/games/trivia/SomeTest.java
@@ -1,6 +1,7 @@
 package com.adaptionsoft.games.trivia;
 
 
+import com.adaptionsoft.games.uglytrivia.Game;
 import org.junit.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -11,4 +12,45 @@ public class SomeTest {
 	public void true_is_true() {
 		assertThat(true).isTrue();
 	}
+
+	@Test
+	public void shouldReturn2When2PlayersAreAdded(){
+		Game game = new Game();
+
+		game.add("playerOne");
+		game.add("playerTwo");
+
+		assertThat(game.howManyPlayers()).isEqualTo(2);
+	}
+
+	@Test
+	public void shouldReturnFalseWhenUserAreOne(){
+		Game game = new Game();
+
+		game.add("playerOne");
+
+		assertThat(game.isPlayable()).isEqualTo(false);
+	}
+
+	@Test
+	public void shouldReturnTrueWhenUsersAreTwo(){
+		Game game = new Game();
+
+		game.add("playerOne");
+		game.add("playerTwo");
+
+		assertThat(game.isPlayable()).isEqualTo(true);
+	}
+	@Test
+	public void shouldReturnTrueWhenUsersAreFour(){
+		Game game = new Game();
+
+		game.add("playerOne");
+		game.add("playerTwo");
+		game.add("playerThree");
+		game.add("playerFour");
+
+		assertThat(game.isPlayable()).isEqualTo(true);
+	}
+
 }
