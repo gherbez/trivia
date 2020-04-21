3787efbe5b3994a5cc17ae9da2911009bf91696e
Gregory HERBEZ <gregory.herbez@accenture.com>
2020-04-21 13:26:29 +0200

    Add GameRunner.java

---
+++ java/src/main/java/com/adaptionsoft/games/runner/GameRunner.java
@@ -0,0 +1,41 @@
+
+package com.adaptionsoft.games.runner;
+import java.util.Random;
+
+import com.adaptionsoft.games.uglytrivia.Game;
+
+
+public class GameRunner {
+
+	private static boolean notAWinner;
+
+	public static Random rand = new Random();
+	public static boolean isTest = false;
+
+	public static void main(String[] args) {
+		Game aGame = new Game();
+		
+		aGame.add("Chet");
+		aGame.add("Pat");
+		aGame.add("Sue");
+
+		if(isTest){
+			rand = new Random(5);
+		}
+
+		do {
+			
+			aGame.roll(rand.nextInt(5) + 1);
+			
+			if (rand.nextInt(9) == 7) {
+				notAWinner = aGame.wrongAnswer();
+			} else {
+				notAWinner = aGame.wasCorrectlyAnswered();
+			}
+			
+			
+			
+		} while (notAWinner);
+		
+	}
+}
